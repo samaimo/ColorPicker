09745b7ae194a4d1ace8e67c0f8120a834b59e01
samaimo <kii328815@gmail.com>
2024-05-30 15:46:39 +0900

    コメントアウト追記

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+// 文字列「カラーコードを検索」を取得
 const text= document.querySelector('#colorText');
+
+// カラーピッカーのコード番号を取得する
 const color = document.querySelector('#colorPicker');
 
+// 「カラーコードを検索」→[カラーコード：#000000]と表示する
 text.textContent = `カラーコード：${color.value}`;
 
