859897bced0e199be231d50cea70c65ab4ae350e
samaimo <kii328815@gmail.com>
2024-05-30 15:43:29 +0900

    定数名をつける

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
-document.querySelector('#colorText').textContent = `カラーコード：${document.querySelector('#colorPicker').value}`;
+const text= document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+text.textContent = `カラーコード：${color.value}`;
 
