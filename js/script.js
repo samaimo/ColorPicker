b5dfa02eeece72c93a55b529e6b6e2a609dfab8d
samaimo <kii328815@gmail.com>
2024-05-30 15:49:39 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ const color = document.querySelector('#colorPicker');
 // 「カラーコードを検索」→[カラーコード：#000000]と表示する
 text.textContent = `カラーコード：${color.value}`;
 
+/*
+実装したいこと
+①（カラーピッカーで）色が
+②選択されたら
+③カラーコードを表示する
+*/
+
+色が.addEventLisner(選択されたら,カラーコードを表示する)；
