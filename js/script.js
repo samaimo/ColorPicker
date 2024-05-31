243cb7f5c6ec48a82977c7dc0aab592e273ab704
samaimo <kii328815@gmail.com>
2024-05-31 10:16:11 +0900

    カラーピッカーで選んだ色を背景に反映

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@ const text= document.querySelector('#colorText');
 // カラーピッカーのコード番号を取得する
 const color = document.querySelector('#colorPicker');
 
-
+// カラーピッカーを操作した時の一連の動作
 const colorBg = () => {
+  // 選択した色を背景色に設定
+  document.body.style.backgroundColor = color.value;
+
+  // カラーコードを表示
   text.textContent = `カラーコード：${color.value}`;
 }
 
